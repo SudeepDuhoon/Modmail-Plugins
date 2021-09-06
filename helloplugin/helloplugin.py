bbbad5309e7bfb4da31130634aecdd9414ffbee3
Sudeep <rishi.gothic@gmail.com>
2021-09-06 21:01:08 +0530

    add few more auto response

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -10,7 +10,11 @@ class HelloPlugin(commands.Cog):
             return
 
         if "hello" in message.content.lower():
-            await message.channel.send("Hey")
+            await message.channel.send("Hey! How are you?")
+        elif "good" in message.content.lower():
+            await message.channel.send("Ah! Glad to hear it.")
+        elif "bruh" in message.content.lower():
+            await message.channel.send("brooooooo")
         elif "yo" in message.content.lower():
             await message.channel.send("yo")
         elif "gm" in message.content.lower():
@@ -23,6 +27,14 @@ class HelloPlugin(commands.Cog):
             await message.channel.send("Good Night !")
         elif "hey" in message.content.lower():
             await message.channel.send("Hello !")
+        elif "sup" in message.content.lower():
+            await message.channel.send("Sup")
+        elif "killzen" in message.content.lower():
+            await message.channel.send("in your dreams lol")
+        elif "dum" in message.content.lower():
+            await message.channel.send("No you are dum")
+        elif "bad" in message.content.lower():
+            await message.channel.send("Why? Tell me?")
         elif "sup" in message.content.lower():
             await message.channel.send("Sup")
         elif "hi" in message.content.lower():
