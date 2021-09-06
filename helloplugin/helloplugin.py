e44e177e5d7d4c18e268dbd4046bf4e5f1ee94cb
Sudeep <rishi.gothic@gmail.com>
2021-09-06 21:29:44 +0530

    Update helloplugin.py

--- helloplugin/helloplugin.py
+++ helloplugin/helloplugin.py
@@ -20,41 +20,39 @@ class HelloPlugin(commands.Cog):
         elif "kidnap" in message.content.lower():
             await message.channel.send("I brought the guns. LETS DO THIS")
         elif "die" in message.content.lower():
-            await message.channel.send("NO")
+            await message.channel.send("I don't have a soul... cries")
         elif "dead" in message.content.lower():
-            await message.channel.send("who isn't bro who isn't")
+            await message.channel.send("Dead? who isn't bro who isn't")
         elif "corpse" in message.content.lower():
             await message.channel.send("RIP")
         elif "kill" in message.content.lower():
-            await message.channel.send("NO")
+            await message.channel.send("NO! em.. may be... yes?")
         elif "cute" in message.content.lower():
-            await message.channel.send("IKR")
+            await message.channel.send("IKR so damn cute uwu")
         elif "bro" in message.content.lower():
             await message.channel.send("brooooooo")
-        elif "zen" in message.content.lower():
-            await message.channel.send("don't tag master")
         elif "chill" in message.content.lower():
-            await message.channel.send("can't chill, too much work")
+            await message.channel.send("can't chill, too much work, unlike you pfft")
         elif "baka" in message.content.lower():
-            await message.channel.send("baka who? where?")
+            await message.channel.send("baka who? where? oh you!")
         elif "good morning" in message.content.lower():
             await message.channel.send("Good Morning !")
         elif "good night" in message.content.lower():
             await message.channel.send("Good Night !")
         elif "hey" in message.content.lower():
             await message.channel.send("yo!  How are you?")
-        elif "sup" in message.content.lower():
-            await message.channel.send("Sup")
         elif "killzen" in message.content.lower():
             await message.channel.send("in your dreams lol")
         elif "kill zen" in message.content.lower():
             await message.channel.send("pfft not gonna happen kiddo")
         elif "dum" in message.content.lower():
-            await message.channel.send("No you are dum")
+            await message.channel.send("Noi. You are dum")
         elif "bad" in message.content.lower():
             await message.channel.send("Why? Tell me?")
+        elif "whatsup" in message.content.lower():
+            await message.channel.send("Nothing much, have a date with my girlfriend. You tell?")
         elif "sup" in message.content.lower():
-            await message.channel.send("Sup")
+            await message.channel.send("Nothing much, have a date with my girlfriend. You tell?")
 
 def setup(bot):
     bot.add_cog(HelloPlugin(bot))
